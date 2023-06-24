da86883ebf5a107525cc83d28de18fe9c37cc7b6
zadolbali10 <anon.yu@mail.ru>
2023-06-24 21:12:12 +0300

    Implement gradient

--- FlopEngine/delaunay_window.hpp
+++ FlopEngine/delaunay_window.hpp
@@ -10,6 +10,7 @@ private:
     static const size_t _agent_count;
     std::vector<utils::agent> _agents;
     triangulation_t _triangulation;
+    std::unordered_map<vector2, std::pair<float, size_t>> _point_weights;
 
 public:
     delaunay_window(int argc, char** argv,
@@ -17,4 +18,9 @@ public:
 
     void physics_loop() override;
     void display() override;
+
+private:
+    void calc_point_weights();
+    void draw_with_edge_length();
+    void draw_with_edge_length_gradient();
 };
