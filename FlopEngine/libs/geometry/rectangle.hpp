082dfb02fb74acb7a6bccecb9a8694c8cd36bc91
zadolbali10 <anon.yu@mail.ru>
2023-07-16 01:59:19 +0300

    Improve rectangle interface

--- FlopEngine/libs/geometry/rectangle.hpp
+++ FlopEngine/libs/geometry/rectangle.hpp
@@ -8,75 +8,113 @@ struct rectangle
     vector2 center;
     vector2 half_dimensions;
 
-    rectangle();
-    rectangle(const vector2& center, const vector2& half_dimensions);
-    rectangle(const vector2& center, float radius);
-    rectangle(const vector2& center, float half_width, float half_height);
+    constexpr rectangle() noexcept;
+    constexpr rectangle(const vector2& center, const vector2& half_dimensions)      noexcept;
+    constexpr rectangle(const vector2& center, float radius)                        noexcept;
+    constexpr rectangle(const vector2& center, float half_width, float half_height) noexcept;
 
-    bool contains(float x, float y) const;
-    bool contains(const vector2& point) const;
-    bool intersects(const rectangle& other) const;
+    constexpr rectangle(const rectangle&) noexcept = default;
+    constexpr rectangle(rectangle&&)      noexcept = default;
 
-    float left() const;
-    float right() const;
-    float top() const;
-    float bot() const;
+    constexpr rectangle& operator=(const rectangle&) noexcept = default;
+    constexpr rectangle& operator=(rectangle&&)      noexcept = default;
 
-    float width() const;
-    float height() const;
+    constexpr bool contains(float x, float y)     const noexcept;
+    constexpr bool contains(const vector2& point) const noexcept;
+
+    constexpr bool intersects(const rectangle& other) const noexcept;
+
+    constexpr float left()  const noexcept;
+    constexpr float right() const noexcept;
+    constexpr float top()   const noexcept;
+    constexpr float bot()   const noexcept;
+
+    constexpr float width()  const noexcept;
+    constexpr float height() const noexcept;
+
     float diagonal() const;
 };
 
-inline bool rectangle::contains(float x, float y) const
+constexpr rectangle::rectangle() noexcept
+    : center{0, 0}
+    , half_dimensions{0, 0}
+{
+
+}
+
+constexpr rectangle::rectangle(const vector2& center, const vector2& half_dimensions) noexcept
+    : center{center}
+    , half_dimensions{half_dimensions}
+{
+
+}
+
+constexpr rectangle::rectangle(const vector2& center, float radius) noexcept
+    : center{center}
+    , half_dimensions{radius, radius}
+{
+
+}
+
+constexpr rectangle::rectangle(const vector2& center, float half_width, float half_height) noexcept
+    : center{center}
+    , half_dimensions{half_width, half_height}
+{
+
+}
+
+constexpr bool rectangle::contains(float x, float y) const noexcept
 {
     return
         math::is_in_range(x, center.x, half_dimensions.x) &&
         math::is_in_range(y, center.y, half_dimensions.y);
 }
 
-inline bool rectangle::contains(const vector2& point) const
+constexpr bool rectangle::contains(const vector2& point) const noexcept
 {
     return contains(point.x, point.y);
 }
 
-inline bool rectangle::intersects(const rectangle& other) const
+constexpr bool rectangle::intersects(const rectangle& other) const noexcept
 {
-    return rectangle(other.center, half_dimensions + other.half_dimensions).contains(center);
+    return 
+        left() < other.right() && other.left() < right() &&
+        bot() < other.top() && other.bot() < top();
 }
 
-inline float rectangle::left() const
+constexpr float rectangle::left() const noexcept
 {
     return center.x - half_dimensions.x;
 }
 
-inline float rectangle::right() const
+constexpr float rectangle::right() const noexcept
 {
     return center.x + half_dimensions.x;
 }
 
-inline float rectangle::top() const
+constexpr float rectangle::top() const noexcept
 {
     return center.y + half_dimensions.y;
 }
 
-inline float rectangle::bot() const
+constexpr float rectangle::bot() const noexcept
 {
     return center.y - half_dimensions.y;
 }
 
-inline float rectangle::width() const
+constexpr float rectangle::width() const noexcept
 {
-    return 2 * half_dimensions.x;
+    return 2.0f * half_dimensions.x;
 }
 
-inline float rectangle::height() const
+constexpr float rectangle::height() const noexcept
 {
-    return 2 * half_dimensions.y;
+    return 2.0f * half_dimensions.y;
 }
 
 inline float rectangle::diagonal() const
 {
-    return 2 * std::sqrtf(
+    return 2.0f * std::sqrtf(
         half_dimensions.x * half_dimensions.x + 
         half_dimensions.y * half_dimensions.y);
 }
